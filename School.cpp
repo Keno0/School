445ee806fd9ad97cb048f6304667deb1619cf64b
Keno0 <norby185@gmail.com>
2017-03-31 17:29:44 +0100

    Add first version

---
+++ School.cpp
@@ -0,0 +1,72 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+
+#include <iostream>
+
+using namespace std;
+
+/**
+prev_max: ha kivettuk az elozo körben az utolsot a tombbol,
+akkor a tomb jelenlegi elso elemet ki kell hagynunk, egyebkent
+azzal kell kezdenunk
+
+inkabb pakoljuk egy uj tombbe
+
+*/
+int drive(int houses[], int prev_max) {
+
+	int newsize = 0;
+	if (prev_max == 1)
+	{
+		cout << houses[0] << endl;
+		return 0;
+	}
+	newsize = prev_max / 2;
+
+	int *newArray = new int[newsize];
+
+	int i = 0;
+	for (int j = 0; j < prev_max; j++)
+	{
+		if (j % 2 == 0)
+		{
+			cout << houses[j] << " ";
+		}
+		else
+		{
+			newArray[i] = houses[j];
+			i++;
+		}
+	}
+
+	drive(newArray, newsize);
+
+}
+
+
+int main()
+{
+
+	// Give number of houses
+	int n;
+	cin >> n;
+
+	int *houses = new int[n];
+	int prev_max = n;
+
+	// Create array with the correct values
+	for (int i = 0; i < n; i++)
+	{
+		houses[i] = i+1;
+	}
+
+	drive(houses, prev_max);
+
+	//delete houses
+	delete[] houses;
+
+	return 0;
+
+}
+
